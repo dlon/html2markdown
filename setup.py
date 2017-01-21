d3b8a4e8e121f39bd0440d58bbf129c2ceab1ad7
David L <dv.lnh.d@gmail.com>
2017-01-21 21:44:39 +0100

    setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(name='html2markdown',
+	py_modules=['html2markdown'],
+	version='0.1',
+	description='Conservatively convert html to markdown',
+	author='dlon',
+	author_email='dv.lnh.d@gmail.com',
+	url='https://github.com/dlon/html2markdown',
+	download_url='https://github.com/dlon/html2markdown/tarball/0.1',
+	requires=['bs4'])
