0ab70f42e241b83c55ba7978abdbf7d5aeb7b1ce
David Lönnhager <dv.lnh.d@gmail.com>
2018-12-10 16:36:13 +0100

    ignore <a> tags without an href attribute

--- html2markdown.py
+++ html2markdown.py
@@ -180,15 +180,15 @@ def _markdownify(tag, _listType=None, _blockQuote=False, _listIndex=1):
 			# process children first
 			for child in children:
 				_markdownify(child)
+			if not tag.has_attr('href'):
+				return
 			if tag.string != tag.get('href') or tag.has_attr('title'):
 				title = ''
-				if tag.has_attr('title') and tag['title']:
+				if tag.has_attr('title'):
 					title = ' "%s"' % tag['title']
 				tag.string = '[%s](%s%s)' % (BeautifulSoup(unicode(tag), 'html.parser').string,
 					tag.get('href', ''),
 					title)
-			elif tag.string == tag.get('href') == tag.get('title') == None:
-				tag.string = '[]()'
 			else:
 				# ! FIXME: hack
 				tag.string = '<<<FLOATING LINK: %s>>>' % tag.string
