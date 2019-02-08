1997da919442ea7d4a424d6308d63a6a85518a3c
David Lönnhager <dv.lnh.d@gmail.com>
2019-02-09 00:33:30 +0100

    Add disclaimer.

--- html2markdown.py
+++ html2markdown.py
@@ -1,6 +1,26 @@
+# -*- coding:utf8 -*-
 """html2markdown converts an html string to markdown while preserving unsupported markup."""
-# TODO:
-#	implement standard <table> (i.e. without attributes)
+#
+# Copyright 2017-2018 David Lönnhager (dlon)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of
+# this software and associated documentation files (the "Software"), to deal in
+# the Software without restriction, including without limitation the rights to
+# use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished
+# to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+# COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+# IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
 import bs4
 from bs4 import BeautifulSoup
 import re
